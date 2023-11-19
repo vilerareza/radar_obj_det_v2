85261aedaa2988023b0906c2f42c093be09c08dc
Reza Vilera <vilera_reza99@yahoo.com>
2023-11-19 14:41:55 +0700

    first commit

--- utils.py
+++ utils.py
@@ -78,7 +78,7 @@ def visualize(img, bboxes, class_id, scores, score_thres, label_dict, color='bgr
 
          # Draw label and score
          class_name = (label_dict[class_id[i]]).strip()
-         result_text = f'{class_name}: {str(scores[i][:4])}'
+         result_text = f'{class_name}: {str(scores[i])[:4]}'
          text_location = (_MARGIN + int(bboxes[i][1]*factor_w),
                         _MARGIN + _ROW_SIZE + int(bboxes[i][0]*factor_h))
          cv2.putText(img, result_text, text_location, cv2.FONT_HERSHEY_PLAIN,
